9d554b89f44b5689305ffb346ed89065ff24e255
LanDev01 <alnjr0001@outlook.com>
2024-01-15 14:10:42 -0300

    developing the first section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 
 import { MenuItem } from "./MenuItem"
 import { Search } from "./Search"
+import { Container } from "./Container"
 
 import Logo from "@/assets/logo.svg"
 import IconUser from "@/assets/icon-user.svg"
@@ -9,8 +10,8 @@ import IconUser from "@/assets/icon-user.svg"
 export function Header() {
     return (
         <header className="relative flex items-center w-full h-20 bg-primary-orange">
-            <div className="absolute top-0 right-0 bg-primary-blue w-[19%] h-full z-10"></div>
-            <div className="flex items-center justify-between w-full max-w-[1246px] px-[15px] mx-auto">
+            <div className="absolute top-0 right-0 bg-primary-blue w-[19%] h-full z-0"></div>
+            <Container>
                 <div className="flex flex-1 items-center justify-between">
                     <div className="flex items-center gap-14">
                         <Image 
@@ -42,14 +43,14 @@ export function Header() {
                     </div>
                   <Search />
                 </div>
-                <button className="flex items-center gap-4 bg-primary-blue h-20 pl-10 z-20">
+                <button className="flex items-center gap-4 bg-primary-blue h-20 pl-10 z-10">
                     <Image 
                         src={IconUser}
                         alt="Icon User"
                     />
                     <span className="text-white font-bold">Acessar conta</span>
                 </button>
-            </div>
+            </Container>
         </header>
     )
 }
